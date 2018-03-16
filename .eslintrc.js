4fd4bb930a8cf9d248fce51a205598bdea705249
Ahmed Abdulrahman <a.kasapbashi@live.com>
2018-03-16 18:10:19 +0100

    :art: User babel module resolver plugin
    User babel module resolver plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,12 @@ module.exports = {
   extends: 'fyndiq',
   rules: {
     'jsx-a11y/anchor-is-valid': 0,
+    'react/jsx-indent': 0,
+    'react/no-array-index-key': 1,
   },
+  settings: {
+    'import/resolver': {
+      'babel-module': {}
+    }
+  }
 }
