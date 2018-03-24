995287ae4126d07bc34de2d4eec2df85eb5c4184
Ahmed Abdulrahman <a.kasapbashi@live.com>
2018-03-24 18:25:11 +0100

    :wrench: Add assetPrefix for gh-pages
    Add assetPrefix for gh-pages

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 const withCSS = require('@zeit/next-css')
 
-module.exports = withCSS()
+const isProduction = process.env.NODE_ENV === 'production'
+
+module.exports = withCSS({
+  exportPathMap() {
+    return {
+      '/': { page: '/' },
+    }
+  },
+  assetPrefix: isProduction ? '/cryptato' : '',
+})
