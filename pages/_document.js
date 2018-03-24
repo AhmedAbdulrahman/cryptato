a875feda05fa6abb67e05f56ace3994943ec5f9e
Ahmed Abdulrahman <a.kasapbashi@live.com>
2018-03-24 20:05:47 +0100

    :wrench: Minor fix
    Minor fix

--- pages/_document.js
+++ pages/_document.js
@@ -8,19 +8,7 @@ export default class MyDocument extends Document {
         <Head>
           <link rel="stylesheet" href="/_next/static/style.css" />
           <link
-            href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700|Roboto:400,500,700"
-            rel="stylesheet"
-          />
-          <link
-            href="https://fonts.googleapis.com/css?family=Jura:400,500|Khand"
-            rel="stylesheet"
-          />
-          <link
-            href="https://fonts.googleapis.com/css?family=Rubik:400,700"
-            rel="stylesheet"
-          />
-          <link
-            href="https://fonts.googleapis.com/css?family=Lato:400,700,900"
+            href="https://fonts.googleapis.com/css?family=Roboto:400,500,700|Lato:400,700,900"
             rel="stylesheet"
           />
         </Head>
